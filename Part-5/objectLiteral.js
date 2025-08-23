cd5dc719c46b41b8406f213b50351bb2aea7e82e
Harshika Gupta <harshikagupta098@gmail.com>
2025-08-24 02:53:18 +0530

    JS PArt-5

---
+++ Part-5/objectLiteral.js
@@ -0,0 +1,18 @@
+let student = {
+    name:"Harshika",
+    age:19,
+    marks:94.4,
+    city:"Delhi"
+    
+};
+
+let student2 = ["Harshika",19,99];
+
+let post = {
+    username: "Harshika",
+    content:"travel",
+    likes:100,
+    reposts:4,
+    tags:2
+};
+
