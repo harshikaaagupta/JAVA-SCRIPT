826197899c161511fd2220a3ac06bad7f4812f97
Harshika Gupta <harshikagupta098@gmail.com>
2025-09-21 13:17:50 +0530

    Arrow functions

--- Part-7/Practice/even.js
+++
@@ -1,9 +0,0 @@
-let n = prompt("Enter the number:");
-let isEven = (n) =>{
-    if(n % 2 ==0){
-        return "Even";
-    }else{
-        return "Odd";
-    }
-};
-console.log(isEven(n));
--- Part-7/app.js
+++ Part-7/app.js
@@ -1,20 +1,65 @@
-const marks={
-    eng:92,
-    math:82,
-    phy:90,
-    getAvg(){
-        let avg = (this.eng+this.math+this.phy)/3;
-        console.log(avg);
-    }
-};
+//this keyword : refers to an object that is executing current piece of code
+// const student={
+//     name:"harshika",
+//     age: 19,
+//     eng:89,
+//     math:90,
+//     chem:95,
+//     getAvg(){
+//         let avg =(this.eng+this.math+this.chem)/3;
+//         console.log(avg);
+//     }
+// }
 
-let square = (n) =>{
-    console.log(n*n);
-};
+// -----------------------------------------------------------------------------------------------------------------------------
+
+//try & catch
+//the try statement allows you to define a block of code to be tsted for errors while it is being executed.
+//the catch statement allows you to define a block of code to be executed,if an error occurs in the try block.
+
+// console.log("hello");
+// console.log("hello");
+// try{
+//     console.log(a);
+// } catch(e){
+//     console.log("caught an error, a is not defined");
+//     console.log(e);
+// }
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+// console.log("hello2");
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+//Miscellaneous Topics
+
+//Arrow Functions
+const sum = (a,b) =>{
+    console.log(a+b);
+}
+
+const cube =(n) =>{
+    console.log(n**3);
+}
+
+//implicit return - to only return a value
+const mul = (a,b)=>(
+    a*b
+);
+
+//Set Timeout - setTimeout(function,timeout) (inbuilt function , and is a window object)
+
+console.log("hi there!");
+setTimeout( () => {
+    console.log("Apna College");
+}, 4000);
+console.log("Welcome to");
+
+//Set Interval - setInterval(function,timeout)
 
 let id = setInterval(() =>{
-    console.log("Hello World");
-}, 2000);
-setTimeout(() =>{
-    clearInterval(id);
-},10000);
+    console.log("Apna College")
+},2000);
+
+console.log(id);
