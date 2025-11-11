dd17c9bffb004f77195afc53937bebb161d5d320
Harshika Gupta <harshikagupta098@gmail.com>
2025-11-11 10:46:24 +0530

    DOM Events

--- Part-10/app.js
+++ Part-10/app.js
@@ -22,25 +22,40 @@ function sayName(){
 //     btn.addEventListener("click",sayName);
 // }
 
+// let btn = document.querySelector("button");
+
+
+// btn.addEventListener("click",function(){
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColour();
+//     h3.innerText = randomColor;
+
+//     let div = document.querySelector("div");
+//     div.style.background = randomColor;
+
+//     console.log("Color updated");
+// });
+
+// function getRandomColour(){
+//   let red = Math.floor(Math.random() * 255);  
+//   let green = Math.floor(Math.random() * 255); 
+//   let black = Math.floor(Math.random() * 255); 
+
+//   let color = `rgb(${red}, ${green}, ${black})`;
+//   return color;
+// }
+
+// let p = document.querySelector("p");
+// p.addEventListener("click",function(){
+//     console.log("para was clicked");
+// })
+
+// let box = document.querySelector("div");
+// box.addEventListener("mouseenter",function(){
+//     console.log("mouse inside div");
+// })
+
 let btn = document.querySelector("button");
-
-
 btn.addEventListener("click",function(){
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColour();
-    h3.innerText = randomColor;
-
-    let div = document.querySelector("div");
-    div.style.background = randomColor;
-
-    console.log("Color updated");
-});
-
-function getRandomColour(){
-  let red = Math.floor(Math.random() * 255);  
-  let green = Math.floor(Math.random() * 255); 
-  let black = Math.floor(Math.random() * 255); 
-
-  let color = `rgb(${red}, ${green}, ${black})`;
-  return color;
-}
+    console.dir(this.innerText);
+})
